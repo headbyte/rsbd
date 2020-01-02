5840269c376ec6b93c51490ac4a80fa432c5c106
headbyte <headbyte@protonmail.com>
2020-01-02 21:33:22 +0000

    Moved hex_dump function to binary.h into binary_tools class as a static function.

--- src/binary.h
+++ src/binary.h
@@ -69,6 +69,47 @@ namespace rsbd {
         }
     };
 
+    struct binary_tools {
+        // Taken from http://www.i42.co.uk/stuff/hexdump.htm
+        template<class E, class T>
+        static inline void hex_dump(const void *data, std::size_t length,
+                                    std::basic_ostream<E, T> &stream, std::size_t columns = 16) {
+            const char *const start = static_cast<const char *>(data);
+            const char *const end = start + length;
+            const char *line = start;
+            while (line != end) {
+                stream.width(4);
+                stream.fill('0');
+                stream << std::hex << line - start << " : ";
+                std::size_t lineLength = std::min(columns, static_cast<std::size_t>(end - line));
+                for (std::size_t pass = 1; pass <= 2; ++pass) {
+                    for (const char *next = line; next != end && next != line + columns; ++next) {
+                        char ch = *next;
+                        switch (pass) {
+                            case 1:
+                                stream << ((ch < 32 || ch > 128) ? '.' : ch);
+                                break;
+                            case 2:
+                                if (next != line)
+                                    stream << " ";
+                                stream.width(2);
+                                stream.fill('0');
+                                stream << std::hex << std::uppercase
+                                       << static_cast<int>(static_cast<unsigned char>(ch));
+                                break;
+                        }
+                    }
+                    if (pass == 1 && lineLength != columns)
+                        stream << std::string(columns - lineLength, ' ');
+                    stream << " ";
+                }
+                stream << std::endl;
+                line = line + lineLength;
+            }
+        }
+
+    };
+
 }
 
 
--- test/test_sync.cpp
+++ test/test_sync.cpp
@@ -11,41 +11,6 @@
 
 using namespace rsbd;
 
-template<class E, class T>
-inline void
-hex_dump(const void *data, std::size_t length, std::basic_ostream<E, T> &stream = std::cout, std::size_t columns = 16) {
-    const char *const start = static_cast<const char *>(data);
-    const char *const end = start + length;
-    const char *line = start;
-    while (line != end) {
-        stream.width(4);
-        stream.fill('0');
-        stream << std::hex << line - start << " : ";
-        std::size_t lineLength = std::min(columns, static_cast<std::size_t>(end - line));
-        for (std::size_t pass = 1; pass <= 2; ++pass) {
-            for (const char *next = line; next != end && next != line + columns; ++next) {
-                char ch = *next;
-                switch (pass) {
-                    case 1:
-                        stream << ((ch < 32 || ch > 128) ? '.' : ch);
-                        break;
-                    case 2:
-                        if (next != line)
-                            stream << " ";
-                        stream.width(2);
-                        stream.fill('0');
-                        stream << std::hex << std::uppercase << static_cast<int>(static_cast<unsigned char>(ch));
-                        break;
-                }
-            }
-            if (pass == 1 && lineLength != columns)
-                stream << std::string(columns - lineLength, ' ');
-            stream << " ";
-        }
-        stream << std::endl;
-        line = line + lineLength;
-    }
-}
 
 TEST_CASE("single file block storage sync test", "[sync]") {
     auto from_memory = std::make_shared<std::stringstream>();
@@ -89,8 +54,8 @@ TEST_CASE("single file block storage sync test", "[sync]") {
     from_memory->read((char *) from_buffer.data(), from_memory_length);
     to_memory->read((char *) to_buffer.data(), to_memory_length);
 
-//    hex_dump(from_buffer.data(), from_buffer.size(), std::cout, 32);
-//    hex_dump(to_buffer.data(), to_buffer.size(), std::cout, 32);
+//    binary_tools::hex_dump(from_buffer.data(), from_buffer.size(), std::cout, 32);
+//    binary_tools::hex_dump(to_buffer.data(), to_buffer.size(), std::cout, 32);
 
     REQUIRE(std::memcmp(from_buffer.data(), to_buffer.data(), from_memory_length) == 0);
 }
